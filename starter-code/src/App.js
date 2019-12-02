359a9c9f48659f766420ec04e596688c97a90825
nurlopez <nlopezcolom@gmail.com>
2019-12-02 17:53:22 +0100

    Fix iteration 1 - update packages via terminal

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 
 
-class App extends React.Component() {
+class App extends React.Component {
     render () {
         return (
             <div>
